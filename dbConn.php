2374c2a72e60e4e50b3ec799966a35c3acd8103a
muhamedd17 <70280821+muhamedd17@users.noreply.github.com>
2021-11-26 13:55:59 -0600

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,15 +1,24 @@
 <?php
 
-  $username='z1905512';
-  $password='1998Aug06';
+  $username1='z1905512';
+  $password1='1998Aug06';
+  $username2='student';
+  $password2='student';
 
   try { // if something goes wrong, an exception is thrown
-    $dsn = "mysql:host=courses;dbname=z1905512";
-    $pdo = new PDO($dsn, $username, $password);
+    $dsn1 = "mysql:host=courses;dbname=z1905512";
+    $pdo1 = new PDO($dsn1, $username1, $password1);
     
   }
-  catch(PDOexception $e) { // handle that exception
-    echo "Connection to database failed: " . $e->getMessage();
+  catch(PDOexception $e1) { // handle that exception
+    echo "Connection to database failed: " . $e1->getMessage();
   }
 
+  try {
+    $dsn2 = "mysql:host=blitz.cs.niu.edu;dbname=csci467";
+    $pdo2 = new PDO($dsn2, $username2, $password2);
+  }
+  catch(PDOexception $e2) {
+    echo "Connection to database failed: " . $e2->getMessage();
+  }
 ?>
