c3d05bf06e8af8f69523e407e6adeb0083e85989
muhamedd17 <70280821+muhamedd17@users.noreply.github.com>
2021-12-05 11:54:38 -0600

    Delete DDLSQL.sql

--- DDLSQL.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS Inventory;   -- Remove tables that's been already created
-DROP TABLE IF EXISTS Ship;
-
-CREATE TABLE Inventory( partNum INT NOT NULL, qty INT, PRIMARY KEY (partNum));
-CREATE TABLE Ship(bracket INT PRIMARY KEY, price FLOAT);
