8e6adffc80758f1c8be50a59c5873cb062f9f504
muhamedd17 <70280821+muhamedd17@users.noreply.github.com>
2021-12-05 13:44:08 -0600

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,5 @@
 DROP TABLE inventory;
+DROP TABLE ship;
 
 CREATE TABLE inventory(partNumber INT NOT NULL, quantity INT, PRIMARY KEY (partNumber));
+CREATE TABLE ship(bracket INT PRIMARY KEY, charges FLOAT);
