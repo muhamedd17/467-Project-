1a5ead8b913f88d54bdc0e4880b9f0c841a456a1
muhamedd17 <70280821+muhamedd17@users.noreply.github.com>
2021-12-05 14:03:59 -0600

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,5 +1,23 @@
 DROP TABLE inventory;
 DROP TABLE ship;
+DROP TABLE index;
+DROP TABLE orderDetail;
 
 CREATE TABLE inventory(partNumber INT NOT NULL, quantity INT, PRIMARY KEY (partNumber));
 CREATE TABLE ship(bracket INT PRIMARY KEY, charges FLOAT);
+CREATE TABLE index(orderNumber INT NOT NULL, partNumber INT NOT NULL, quantity INT, PRIMARY KEY (orderNumber, partNumber), FOREIGN KEY (orderNumber) REFERENCES orderDetail(customerID));
+
+CREATE TABLE orderDetail(
+    customerID INT NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(30) NOT NULL,
+    address    VARCHAR(60) NOT NULL,
+    email      VARCHAR(30) NOT NULL,
+    cardNumber VARCHAR(16) NOT NULL,
+    cardExpire DATE     NOT NULL,
+    complete   INT     NOT NULL DEFAULT 0,
+    weight     INT     NOT NULL DEFAULT 0,
+    dateSubmit DATE    NOT NULL,
+    total      DECIMAL(12,2) NOT NULL DEFAULT 0,
+    
+    PRIMARY KEY (customerID)
+);
