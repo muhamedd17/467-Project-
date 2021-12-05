55ddf0a2559b961ccd1df4b315519f7fe88355d8
muhamedd17 <70280821+muhamedd17@users.noreply.github.com>
2021-12-05 13:19:25 -0600

    Create orderConfirm.php

---
+++ orderConfirm.php
@@ -0,0 +1,8 @@
+<!-- Muhamed Delic -->
+<!-- z1905512 -->
+<!-- CSCI 467 -->
+<!-- Project Part 3 -->
+<html>
+    <h1>Your Order Has Been Placed</h1>
+    <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
+</html>
