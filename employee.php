0c9ff9c3ebbb5ee95b185d19ce4a091efff8bbca
muhamedd17 <70280821+muhamedd17@users.noreply.github.com>
2021-12-05 20:02:42 -0600

    Create employee.php

---
+++ employee.php
@@ -0,0 +1,17 @@
+<!-- Muhamed Delic -->
+<!-- z1905512 -->
+<!-- CSCI 467 -->
+<!-- Project Part 3 -->
+<html><head><title> NIU Auto Parts Employee UI </title></head>
+<body style="background-color:Tomato;">
+<h1>Choose which employee job you are assigned to:</h1>
+<form action="http://students.cs.niu.edu/~z1905512/employee.php" method="POST">
+<input type="radio" id="workstation" name="employee" value="workstation">
+<label for="work">Workstation Employee</label><br>
+<input type="radio" id="receiving" name="employee" value="receiving">
+<label for="emp2">Receiving Desk Employee</label><br>
+<input type="radio" id="admin" name="employee" value="admin">
+<label for="emp3">Administrator Employee</label><br>
+<input type="submit" value="Submit">
+</body>
+</html>
