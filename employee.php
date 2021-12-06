549186f7460d575a6a3c3f544b390775132de989
muhamedd17 <70280821+muhamedd17@users.noreply.github.com>
2021-12-05 20:42:19 -0600

    Update employee.php

--- employee.php
+++ employee.php
@@ -4,14 +4,15 @@
 <!-- Project Part 3 -->
 <html><head><title> NIU Auto Parts Employee UI </title></head>
 <body style="background-color:Tomato;">
-<h1>Choose which employee job you are assigned to:</h1>
-<form action="http://students.cs.niu.edu/~z1905512/employee.php" method="POST">
-<input type="radio" id="workstation" name="employee" value="workstation">
-<label for="work">Workstation Employee</label><br>
-<input type="radio" id="receiving" name="employee" value="receiving">
-<label for="emp2">Receiving Desk Employee</label><br>
-<input type="radio" id="admin" name="employee" value="admin">
-<label for="emp3">Administrator Employee</label><br>
-<input type="submit" value="Submit">
+<h1>Choose which employee you are:</h1>
+
+<?
+echo '<form method='POST'action='employee.php'>
+ <input type='submit' name='workstation' value='Workstation Employee' style=float:left />
+ <form method='POST'action='receiving.php'>
+ <input type='submit' name='receiving' value='Receiving Desk Employee' style=float:left />
+ <input type='submit' name='admin' value='Administrator Employee' style=float:left />
+ ';
+?>
 </body>
 </html>
