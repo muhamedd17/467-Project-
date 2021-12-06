f2c58f909a981cde2104cf2b95494df6c17e61bb
kevingeb <70212898+kevingeb@users.noreply.github.com>
2021-12-06 15:37:09 -0600

    Update workstation.php

--- workstation.php
+++ workstation.php
@@ -1,1 +1,23 @@
+<html>
+	<head>
+		<title>
+		Warehouse interface
+		</title>
+	</head>
 
+	<h1 style="text-align:left">
+	Warehouse Workstation
+	</h1>
+
+	<body style="background-color:Tomato;">
+		<h2>
+		Submitted orders
+		</h2>
+		<a href="">ORDER 0001</a>
+		<a href="">ORDER 0002</a>
+		<a href="">ORDER 0003</a>
+		<a href="">ORDER 0004</a>
+		<a href="">ORDER 0005</a>
+		<a href="">ORDER 0006</a>	
+	</body>
+</html>
