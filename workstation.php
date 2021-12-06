60e034dfe3a56f063a0e29fc73b08efd1800f6b4
kevingeb <70212898+kevingeb@users.noreply.github.com>
2021-12-06 15:55:47 -0600

    Update workstation.php

--- workstation.php
+++ workstation.php
@@ -13,11 +13,11 @@
 		<h2>
 		Submitted orders
 		</h2>
-		<a href="">ORDER 0001</a>
-		<a href="">ORDER 0002</a>
-		<a href="">ORDER 0003</a>
-		<a href="">ORDER 0004</a>
-		<a href="">ORDER 0005</a>
-		<a href="">ORDER 0006</a>	
+		<a href="http://students.cs.niu.edu/~z1905512/order1">ORDER 0001</a>
+		<a href="http://students.cs.niu.edu/~z1905512/order2">ORDER 0002</a>
+		<a href="http://students.cs.niu.edu/~z1905512/order3">ORDER 0003</a>
+		<a href="http://students.cs.niu.edu/~z1905512/order4">ORDER 0004</a>
+		<a href="http://students.cs.niu.edu/~z1905512/order5">ORDER 0005</a>
+		<a href="http://students.cs.niu.edu/~z1905512/order6">ORDER 0006</a>	
 	</body>
 </html>
