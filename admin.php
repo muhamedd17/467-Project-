5361413944e870432747ef8cd124e4d2c00af10e
vlad2oprescu <55206769+vlad2oprescu@users.noreply.github.com>
2021-12-06 15:30:45 -0600

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,45 @@
+<!-- Vlad Oprescu -->
+<!-- z1665498 -->
+<!-- CSCI 467 -->
+<!-- Project Part 3 -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href=
+    "https://lh3.googleusercontent.com/ej6MYUL3K8rVH3fe1BaXkykOx1xnzdaUdtW89Cf8Axm8yrqEm_wMgRT83U7YNjECCSAu2e53nYCPikeiAym-GLKChNJbNUO1pQ8L-7CRkpf4FIQiTTJhkMshxCtTDf_ifP9Y6lbW_A=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/ej6MYUL3K8rVH3fe1BaXkykOx1xnzdaUdtW89Cf8Axm8yrqEm_wMgRT83U7YNjECCSAu2e53nYCPikeiAym-GLKChNJbNUO1pQ8L-7CRkpf4FIQiTTJhkMshxCtTDf_ifP9Y6lbW_A=w600-h315-p-k" /> </a>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="login.css">
+    <title>Login Page</title>
+</head>
+  
+<body>
+    <form action="validate.php" method="post">
+        <div class="login-box">
+            <h1>Login</h1>
+  
+            <div class="textbox">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <input type="text" placeholder="Adminname"
+                         name="adminname" value="">
+            </div>
+  
+            <div class="textbox">
+                <i class="fa fa-lock" aria-hidden="true"></i>
+                <input type="password" placeholder="Password"
+                         name="password" value="">
+            </div>
+  
+            <input class="button" type="submit"
+                     name="login" value="Sign In">
+        </div>
+    </form>
+</body>
+  
+</html>
+   
