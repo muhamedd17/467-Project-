57eba9e894993c5c41ad9f77e62b6f1d5ebd69f3
vlad2oprescu <55206769+vlad2oprescu@users.noreply.github.com>
2021-12-06 20:30:21 -0600

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Administrator Log In</title>
+</head>
+<body>
+<div class="header">
+	<h2>Register</h2>
+</div>
+<form method="post" action="register.php">
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="">
+	</div>
+	<div class="input-group">
+		<label>password1</label>
+		<input type="password" name="Password">
+	<div class="input-group">
+		<button type="submit" class="btn" name="register_btn">Register</button>
+	</div>
+	
+</form>
+</body>
+</html>
